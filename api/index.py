c8047833658ebb8bed07c2c6b66540d3114c6810
VK-JPG-cmd <vk-jpg-cmd@users.noreply.github.com>
2026-09-16 11:52:26 +0530

    Remove backend files from git repository

--- api/index.py
+++
@@ -1,3 +0,0 @@
-from src.api import app
-
-# Vercel entrypoint
--- main.py
+++
@@ -1,6 +0,0 @@
-from src.api import app
-
-if __name__ == "__main__":
-    import uvicorn
-    from src.config import settings
-    uvicorn.run("main:app", host=settings.HOST, port=settings.PORT, reload=True)
