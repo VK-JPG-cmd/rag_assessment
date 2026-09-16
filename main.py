44be47cb0468090199beefe4db231784cc7a2dbd
VK-JPG-cmd <vk-jpg-cmd@users.noreply.github.com>
2026-09-16 12:03:06 +0530

    Include source files and Render configuration for deployment

---
+++ main.py
@@ -0,0 +1,6 @@
+from src.api import app
+
+if __name__ == "__main__":
+    import uvicorn
+    from src.config import settings
+    uvicorn.run("main:app", host=settings.HOST, port=settings.PORT, reload=True)
